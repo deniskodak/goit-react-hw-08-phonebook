6770b19470b0eb53e1d164f6a0918d91f083bcf7
deniskodak <denis.kodak@gmail.com>
2021-07-26 00:09:28 +0300

    add avatar to logined user

--- src/components/userComponents/UserMenu/UserMenu.js
+++ src/components/userComponents/UserMenu/UserMenu.js
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 
 import styles from './UserMenu.module.scss';
 
-const UserMenu = ({ user, onLogout }) => {
+const UserMenu = ({ avatar, user, onLogout }) => {
   return (
     <div className={styles.container}>
+      <img
+        src={avatar}
+        alt="user avatar"
+        width="32"
+        className={styles.avatar}
+      />
       <span className={styles.greetings}>Glad to see you,</span>
       <span className={styles.name}>{user.name}</span>
       <button
